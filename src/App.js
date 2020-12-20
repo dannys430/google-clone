4f9073b76d44d46ebdd66d4c5c540dbbeeb9f03b
Daniel Singer <dannys430@yahoo.com>
2020-12-19 19:34:48 -0500

    added components and pages folders and home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home'
+
 
 function App() {
   return (
     <div className="app">
       <h1>hey from app.js</h1>
+
+      {/* Home */}
+      <Home />
+
+      {/* Search Results */}
+
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>hey from Home</h1>
+      
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* about link */}
+          {/* store link */}
+
+        </div>
+        <div className="home__headerRight">
+          {/* gmail link */}
+          {/* images link */}
+          {/* grid link */}
+          {/* avatar */}
+        </div>
+      </div>
+      
+      <div className="home__body">
+        
+      </div>
+       
+    </div>
+  )
+}
+
+export default Home
