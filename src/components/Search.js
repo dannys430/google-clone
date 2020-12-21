e074087154f1bde28770894e874ce90b2860ac76
Daniel Singer <dannys430@yahoo.com>
2020-12-21 16:21:14 -0500

    finished setup of React Context API

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,12 @@ import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import {Button} from '@material-ui/core';
 import {useHistory} from 'react-router-dom';
+import {useStateValue} from '../StateProvider';
+import {actionTypes} from '../reducer';
 
 function Search({hideButtons = false}) {
+  const [{}, dispatch] = useStateValue();
+  
   const [input, setInput] = useState('')
   const history = useHistory();
 
@@ -14,6 +18,11 @@ function Search({hideButtons = false}) {
     e.preventDefault();
 
     console.log(`you hit search.  your query is: ${input}`)
+
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input
+    })
 
     history.push('/search')
     
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer, {initialState} from './reducer';
+import {StateProvider} from './StateProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App /> {/* here we are wrapping App inside of a data layer (StateProvider)*/}
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
